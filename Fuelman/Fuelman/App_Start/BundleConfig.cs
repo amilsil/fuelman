8248039d76cff9d0dcb621ddafb9d8de26871ad8
amilsil <amilsil@gmail.com>
2014-11-18 16:53:39 +0530

    Added bootstrap integration

--- Fuelman/Fuelman/App_Start/BundleConfig.cs
+++ Fuelman/Fuelman/App_Start/BundleConfig.cs
@@ -24,6 +24,11 @@ namespace Fuelman
                     "~/Scripts/angular-route.js"
                 ));
 
+            // bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js"
+                ));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
