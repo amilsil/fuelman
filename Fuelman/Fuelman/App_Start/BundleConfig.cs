bfb68ac860c15c958c5a58ebeb70c7911bae0370
amilsil <amilsil@gmail.com>
2014-11-19 13:40:56 +0530

    Adding bootstrap

--- Fuelman/Fuelman/App_Start/BundleConfig.cs
+++ Fuelman/Fuelman/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Fuelman
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.11.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
